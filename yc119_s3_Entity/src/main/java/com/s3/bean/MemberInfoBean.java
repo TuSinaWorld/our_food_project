c03a1f210cd46088fc9dfc0071e3124e1d4c91df
tanyaoli <1607667368@qq.com>
2023-05-05 21:02:48 +0800

    实体类上传&&login模块创建

---
+++ yc119_s3_Entity/src/main/java/com/s3/bean/MemberInfoBean.java
@@ -0,0 +1,24 @@
+package com.s3.bean;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+
+//用户表
+@Data
+@TableName("memberinfo")
+public class MemberInfoBean {
+    @TableId(type = IdType.AUTO)
+    private Integer mno;
+    private String nickName;
+    private String realName;
+    private String pwd;
+    private String tel;
+    private String email;
+    private String photo;
+    private String regDate;
+    private Integer status;
+    private boolean ban = false;
+}
---
+++ yc119_s3_Login/src/test/java/com/s3/Test.java
@@ -0,0 +1,8 @@
+package com.s3;
+
+public class Test {
+    @org.junit.jupiter.api.Test
+    public void test(){
+       System.out.println();
+    }
+}
