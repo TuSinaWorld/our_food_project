0e15b1c04c548fb906c121448ca4fe37ff6ac3cf
tanyaoli <1607667368@qq.com>
2023-05-05 21:42:10 +0800

    添加配置信息 测试数据库

---
+++ yc119_s3_Login/src/main/java/com/s3/common/dto/Resuser.java
@@ -0,0 +1,5 @@
+package com.s3.common.dto;
+
+public class Resuser {
+//    private String
+}
---
+++ yc119_s3_Login/src/main/java/com/s3/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.s3.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.s3.bean.MemberInfoBean;
+
+public interface UserMapper extends BaseMapper<MemberInfoBean> {
+
+}
